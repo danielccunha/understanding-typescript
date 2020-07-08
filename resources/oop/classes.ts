29210e211d21332b95312efa3daf1fc59630a591
Daniel Cunha <danielcunha54@gmail.com>
2020-07-08 13:37:30 -0300

    refactor: Moved classes file to resources/oop folder

---
+++ resources/oop/classes.ts
@@ -0,0 +1,86 @@
+abstract class Department {
+  static readonly fiscalYear = 2020;
+  protected employees: string[];
+
+  constructor(private readonly id: string, public name: string) {
+    this.employees = [];
+  }
+
+  static createEmployee = (name: string) => ({ name });
+
+  abstract describe(): void;
+
+  addEmployee(name: string) {
+    this.employees.push(name);
+  }
+}
+
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'Tech');
+  }
+
+  describe(): void {
+    console.log('IT Department');
+  }
+}
+
+class AccountingDepartment extends Department {
+  private static _instance: AccountingDepartment;
+  private _lastReport: string;
+
+  get lastReport() {
+    if (this._lastReport) {
+      return this._lastReport;
+    }
+
+    throw new Error('No report found.');
+  }
+
+  set lastReport(value: string) {
+    this._lastReport = value;
+  }
+
+  private constructor(id: string, private reports: string[] = []) {
+    super(id, 'Accounting');
+    this._lastReport = reports.length ? reports[reports.length - 1] : '';
+  }
+
+  static get instance() {
+    if (!this._instance) {
+      this._instance = new AccountingDepartment('ACC', []);
+    }
+
+    return this._instance;
+  }
+
+  describe(): void {
+    console.log('Accounting Department');
+  }
+
+  addEmployee(name: string) {
+    if (this.employees.some((n) => n === name)) {
+      return;
+    }
+
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('IT', ['Daniel']);
+console.log(it);
+it.describe();
+
+const accounting = AccountingDepartment.instance;
+accounting.lastReport = 'Some random report';
+console.log(accounting, accounting.lastReport);
+
+console.log(Department.createEmployee('Daniel'));
