273632dee02dbffb7091bc531d67951c0d716481
Daniel Cunha <danielcunha54@gmail.com>
2020-07-13 14:03:04 -0300

    feat: Learnt type guards

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,9 @@ type Employee = {
   startDate: Date;
 };
 
+// Intersection Types
 type ElevatedEmployee = Admin & Employee;
+type UnknownEmployee = Employee | Admin;
 
 const employee: ElevatedEmployee = {
   name: 'Daniel',
@@ -16,6 +18,48 @@ const employee: ElevatedEmployee = {
   startDate: new Date(),
 };
 
-type Combinable = string | number;
-type Numeric = number | boolean;
-type Universal = Combinable & Numeric;
+// Type Guard
+function printEmployee(employee: UnknownEmployee) {
+  console.log(`Name: ${employee.name}`);
+
+  if ('privileges' in employee) {
+    console.log(`Privileges: ${employee.privileges || []}`);
+  }
+
+  if ('startDate' in employee) {
+    console.log(`Start date: ${employee.startDate || 'N/D'}`);
+  }
+}
+
+printEmployee(employee);
+
+class Car {
+  drive() {
+    console.log('Driving a car...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo: ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+const car = new Car();
+const truck = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(car);
+useVehicle(truck);
