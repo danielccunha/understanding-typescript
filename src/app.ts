b3dbc1a7be8cbe823a4bd9981af3ebfa49883f2e
Daniel Cunha <danielcunha54@gmail.com>
2020-07-15 13:22:32 -0300

    feat: Started studying generics

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hey there');
+// const names: string[] = ['Daniel', 'Isabella'];
+const names: Array<string> = ['Daniel', 'Isabella'];
+console.log(names);
+
+const promise = new Promise<string>((resolve, _reject) => {
+  setTimeout(() => resolve('This is done!'), 1000);
+});
+
+promise.then(console.log);
