91a828ad4b73ce6dbd58625790d5cb3e074d50d1
Daniel Cunha <danielcunha54@gmail.com>
2020-07-20 22:49:04 -0300

    feat: Learnt utility types

---
+++ resources/generics/utility-types.ts
@@ -0,0 +1,21 @@
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Daniel', 'Bella'];
+// names.push('Luiza');
